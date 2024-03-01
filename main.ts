a0305504fe576f50b1709b33878da445f921bdcf
Brandon Philips <brandon@ifup.org>
2024-03-01 12:42:02 -0800

    initial working commit

--- main.ts
+++ main.ts
@@ -1,52 +1,56 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
-import { SupernoteX, toSharp } from 'supernote/src';
+import { App, TFolder, TFile, Plugin, PluginSettingTab, Setting, TAbstractFile } from 'obsidian';
+import { SupernoteX, toImage } from 'supernote-typescript';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface SupernotePluginSettings {
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: SupernotePluginSettings = {
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+
+function toBuffer(arrayBuffer: ArrayBuffer) {
+	const buffer = Buffer.alloc(arrayBuffer.byteLength);
+	const view = new Uint8Array(arrayBuffer);
+	for (let i = 0; i < buffer.length; ++i) {
+		buffer[i] = view[i];
+	}
+	return buffer;
+}
+
+export default class SupernotePlugin extends Plugin {
+	settings: SupernotePluginSettings;
 
 	async onload() {
 		await this.loadSettings();
+		this.registerEvent(this.app.vault.on("create", async (file) => {
+			if (file instanceof TFolder) {
+				return;
+			}
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerInterval(window.setInterval(() => this.findNotes(), 5 * 1000));
-		await this.findNotes();
+			if (file.path.endsWith('.note')) {
+				console.log(`got ${file.path}`);
+				const note = await this.app.vault.readBinary(file as TFile);
+				let sn = new SupernoteX(toBuffer(note));
+				let images = await toImage(sn);
+				console.log(`image.length ${images.length}`)
+				for (let i = 0; i < images.length; i++) {
+					let filename = `${file.path}-${i}.png`
+					if (this.app.vault.getFileByPath(filename)) {
+						console.log(`skipped ${filename}`);
+						continue;
+					}
+					this.app.vault.createBinary(filename, images[i].toBuffer());
+					console.log(`wrote ${filename}`);
+				}
+			}
+		}));
 	}
 
 	onunload() {
 
 	}
-
-	async findNotes() {
-		const files = this.app.vault.getFiles();
-		for (const file of files) {
-			if (file.path.endsWith('.note')) {
-				console.log('Found note file:', file.path);
-				const note = this.app.vault.cachedRead(file);
-				let sn = new SupernoteX(note);
-				let images = await toSharp(sn)
-				for await (const [index, image] of images.entries()) {
-				  await this.app.vault.createBinary(`${file.path}-${index}.png`, image.toBuffer());
-				}
-			}
-		}
-	}
-
-	async createImage() {
-
-	}
-
 
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
@@ -54,31 +58,5 @@ export default class MyPlugin extends Plugin {
 
 	async saveSettings() {
 		await this.saveData(this.settings);
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
