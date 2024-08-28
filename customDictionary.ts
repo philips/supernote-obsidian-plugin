9581bb59a241be485227727877bc1a1283e3aa69
ssylvia <ssylvia@email.com>
2024-08-28 14:22:58 -0400

    Use array instead of unique ID

--- customDictionary.ts
+++ customDictionary.ts
@@ -1,5 +1,4 @@
 import SupernotePlugin from "./main";
-import { randomUUID } from 'crypto';
 
 type CustomDictionaryEntry = {
 	src: string,
@@ -8,19 +7,15 @@ type CustomDictionaryEntry = {
 
 export interface CustomDictionarySettings {
 	isCustomDictionaryEnabled: boolean,
-	customDictionary: Record<string, CustomDictionaryEntry>
+	customDictionary: CustomDictionaryEntry[]
 }
 
 export const CUSTOM_DICTIONARY_DEFAULT_SETTINGS: CustomDictionarySettings = {
 	isCustomDictionaryEnabled: false,
-	customDictionary: {},
+	customDictionary: [],
 }
 
-function getDictionaryEntryId(): string {
-	return randomUUID();
-}
-
-function createDictionaryEntryUI({id, entry, tbody, plugin}: {id: string, entry: CustomDictionaryEntry, tbody: HTMLElement, plugin: SupernotePlugin}) {	
+function createDictionaryEntryUI({entry, tbody, plugin}: {entry: CustomDictionaryEntry, tbody: HTMLElement, plugin: SupernotePlugin}) {	
 	const tr = tbody.createEl('tr');
 
 	// Source Text Input
@@ -41,7 +36,11 @@ function createDictionaryEntryUI({id, entry, tbody, plugin}: {id: string, entry:
 
 	// Update dictionary entry when input changes
 	const updateDictionaryEntry = async () => {
-		plugin.settings.customDictionary[id] = {src: sourceInput.value, replace: replaceInput.value};
+		const index = Array.from(tbody.children).indexOf(tr)
+		plugin.settings.customDictionary[index] = {
+			src: sourceInput.value,
+			replace: replaceInput.value
+		};
 		await plugin.saveSettings();
 	}
 	sourceInput.addEventListener('input', updateDictionaryEntry);
@@ -49,10 +48,54 @@ function createDictionaryEntryUI({id, entry, tbody, plugin}: {id: string, entry:
 
 	// Delete dictionary entry when delete button is clicked
 	deleteButton.addEventListener('click', async () => {
-		delete plugin.settings.customDictionary[id];
+		const index = Array.from(tbody.children).indexOf(tr);
+		plugin.settings.customDictionary.splice(index, 1);
 		await plugin.saveSettings();
 		tr.remove();
 	});
+}
+
+function createDictionaryTableUI(containerEl: HTMLElement, plugin: SupernotePlugin) {
+	const CONTAINER_CLASSNAME = 'supernote-settings-custom-dictionary-entries';
+
+	// Remove existing dictionary entries to prepare for re-render
+	containerEl.find(`.${CONTAINER_CLASSNAME}`)?.remove();
+
+	const dictionaryEntriesContainer = containerEl.createDiv();
+	const table = dictionaryEntriesContainer.createEl('table');
+	const thead = table.createEl('thead');
+	const trHead = thead.createEl('tr');
+	const tbody = table.createEl('tbody');
+
+	dictionaryEntriesContainer.addClass(CONTAINER_CLASSNAME);
+	trHead.createEl('th', { text: 'Source Text' });
+	trHead.createEl('th', { text: 'Replace Text' });
+	trHead.createEl('th', { text: 'Options' });
+
+
+	const addEmptyRow = () => {
+		createDictionaryEntryUI({
+			entry: {src: '', replace: ''},
+			tbody,
+			plugin
+		});
+	}
+
+
+	for (const entry of plugin.settings.customDictionary) {
+		createDictionaryEntryUI({
+			entry,
+			tbody,
+			plugin
+		});
+	}
+
+	if (plugin.settings.customDictionary.length === 0) {
+		addEmptyRow();
+	}
+
+	const addEntryButton = dictionaryEntriesContainer.createEl('button', { text: 'Add dictionary entry' });
+	addEntryButton.addEventListener('click', addEmptyRow);
 }
 
 export function createCustomDictionarySettingsUI(containerEl: HTMLElement, plugin: SupernotePlugin): void {
@@ -64,40 +107,7 @@ export function createCustomDictionarySettingsUI(containerEl: HTMLElement, plugi
 	customDictionaryContainer.createDiv({ text: 'You can add custom entries to your dictionary to fix errors from Supernote\'s handwriting recognition. This also lets you automatically swap out certain text with your preferred wording.' })
 		.addClass('setting-item-description');
 
-	const table = customDictionaryContainer.createEl('table');
-	const thead = table.createEl('thead');
-	const trHead = thead.createEl('tr');
-	trHead.createEl('th', { text: 'Source Text' });
-	trHead.createEl('th', { text: 'Replace Text' });
-	trHead.createEl('th', { text: 'Options' });
-
-	const tbody = table.createEl('tbody');
-
-	const addEmptyRow = () => {
-		createDictionaryEntryUI({
-			id: getDictionaryEntryId(),
-			entry: {src: '', replace: ''},
-			tbody,
-			plugin
-		});
-	}
-
-
-	for (const [id, entry] of Object.entries(plugin.settings.customDictionary)) {
-		createDictionaryEntryUI({
-			id,
-			entry,
-			tbody,
-			plugin
-		});
-	}
-
-	if (Object.keys(plugin.settings.customDictionary).length === 0) {
-		addEmptyRow();
-	}
-
-	const addEntryButton = customDictionaryContainer.createEl('button', { text: 'Add dictionary entry' });
-	addEntryButton.addEventListener('click', addEmptyRow);
+	createDictionaryTableUI(customDictionaryContainer, plugin);
 }
 
 function escapeRegExp(string: string): string {
